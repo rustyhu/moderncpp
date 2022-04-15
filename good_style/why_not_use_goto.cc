faa87f3648004773d8b79bfe0eb11315c983c5a5
rustyhu <rustyhu@hotmail.com>
2022-04-15 15:41:24 +0800

    Add good_style case: why_not_use_goto;

---
+++ good_style/why_not_use_goto.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int frontFun(bool errhappen) {
+  if (errhappen) {
+    // Problem here:
+    // transfer of control bypasses initialization of: variables
+    // goto end;
+  }
+
+  int a{32};
+  char tmpstr[64] = {0};
+  std::vector<int32_t> col;
+
+end:
+  std::cout << "reach end!\n";
+}
+
+int frontFunSec(bool errhappen) {
+  do {
+    if (errhappen) {
+      break;
+    }
+
+    int a{32};
+    char tmpstr[64] = {0};
+    std::vector<int32_t> col;
+
+    // do something to these variables, like:
+    std::cout << a << tmpstr << "\n";
+  } while (0);
+
+  std::cout << "reach end!\n";
+}
